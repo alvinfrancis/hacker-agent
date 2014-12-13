1af81ef77480748860fc825743a93a9d3ecec4ac
alvinfrancis <alvinfrancisdumalus@gmail.com>
2014-12-13 22:12:57 +0800

    Update to reagent 0.5.0

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :source-paths ["src/clj" "src/cljs"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [reagent "0.4.3"]
+                 [reagent "0.5.0-alpha"]
                  [reagent-utils "0.1.0"]
                  [secretary "1.2.0"]
                  [org.clojure/clojurescript "0.0-2371" :scope "provided"]
